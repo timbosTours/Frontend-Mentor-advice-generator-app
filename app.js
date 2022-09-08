4bed5ac1324c7648e755d645ca7039d11ffa5e7d
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-09-09 06:57:05 +1000

    cerated async/await function to get advice

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // fetch('https://api.adviceslip.com/advice')
 //     .then(res => res.json())
 //     .then(data => console.log(data.slip.advice))
+async function getAdvice(){
+        const response = await fetch('https://api.adviceslip.com/advice');
+
+        const data = await response.json();
+    
+        const adviceId = data.slip.id;
+        const advice = data.slip.advice;
+
+        console.log(adviceeId);
+        console.log(advice);
+        return adviceId, advice;
+}
+
+getAdvice();
