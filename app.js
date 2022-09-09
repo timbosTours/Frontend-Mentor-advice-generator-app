f2fdc25543ca6ea0142331040bbeee1f144dfb93
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-09-10 09:56:42 +1000

    added document.fragment to function to append advice to html

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-// fetch('https://api.adviceslip.com/advice')
-//     .then(res => res.json())
-//     .then(data => console.log(data.slip.advice))
-async function getAdvice(){
+// Create an async function to retrive json data, convert it and display it on screen
+async function getAdvice() {
+    try {
+        // get data
         const response = await fetch('https://api.adviceslip.com/advice');
-
+        // convert data
         const data = await response.json();
-    
-        const adviceId = data.slip.id;
-        const advice = data.slip.advice;
+        // grab datapoints an asign to variables
+        const adviceId = await data.slip.id;
+        const advice = await data.slip.advice;
+        // grab element to append datapoints too
+        const body = document.getElementById('body');
+        // create empty document fragment
+        const docFrag = document.createDocumentFragment();
+        // create new HTML elements to append datapoints too
+        let item1 = document.createElement('h1')
+        let item2 = document.createElement('h2')
+        // asign datapoint variables to items
+        item1.textContent = adviceId;
+        item2.textContent = advice;
+        // append to empty document fragment
+        docFrag.appendChild(item1);
+        docFrag.appendChild(item2);
+        // append doc frag to DOM element
+        body.appendChild(docFrag);
 
-        console.log(adviceeId);
-        console.log(advice);
-        return adviceId, advice;
+    } catch (error) {
+        console.log('Error!');
+    }
 }
 
 getAdvice();
